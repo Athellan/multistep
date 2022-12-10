2a1158611b16f5c2fc5596c855ed27f17b28e0d8
Jean-Baptiste CAMBILLARD <jeanbaptiste.cambillard@gmail.com>
2022-12-10 15:52:14 +0100

    transitions + mapping ok

--- src/Components/MultiForm/Multiform.js
+++ src/Components/MultiForm/Multiform.js
@@ -30,10 +30,27 @@ export default function Multiform() {
     }
   };
 
+  const elements = [
+    <CardBegin modifyIndex={modifyIndex} />,
+    <DietForm modifyIndex={modifyIndex} />,
+    <FoodStyle modifyIndex={modifyIndex} />,
+    <Allergies modifyIndex={modifyIndex} />,
+    <HateLove modifyIndex={modifyIndex} />,
+    <CardEnd modifyIndex={modifyIndex} />,
+  ];
+
   return (
     <div className="container-multiform">
-      <Indicator />
-      {formIndex === 1 ? (
+      <Indicator formIndex={formIndex} />
+
+      {elements.map((item, index) => {
+        if (index + 1 === formIndex) {
+          return elements[index];
+        }
+      })}
+
+      {/* Test ternaire OK */}
+      {/* {formIndex === 1 ? (
         <CardBegin modifyIndex={modifyIndex} />
       ) : formIndex === 2 ? (
         <DietForm modifyIndex={modifyIndex} />
@@ -43,10 +60,11 @@ export default function Multiform() {
         <Allergies modifyIndex={modifyIndex} />
       ) : formIndex === 5 ? (
         <HateLove modifyIndex={modifyIndex} />
+      ) : formIndex === 6 ? (
+        <CardEnd modifyIndex={modifyIndex} />
       ) : (
         ""
-      )}
-      {/* <CardEnd /> */}
+      )} */}
     </div>
   );
 }
