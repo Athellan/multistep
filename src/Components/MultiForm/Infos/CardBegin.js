2cc9db98624d013bda3e3883a84be9590d4917af
Jean-Baptiste CAMBILLARD <jeanbaptiste.cambillard@gmail.com>
2022-12-11 13:59:37 +0100

    modif card

--- src/Components/MultiForm/Infos/CardBegin.js
+++ src/Components/MultiForm/Infos/CardBegin.js
@@ -4,7 +4,7 @@ import "./Card.css";
 export default function CardBegin(props) {
   return (
     <div className="card">
-      <h1>🍱Aide-nous à ravir tes papilles🌶️</h1>
+      <h1>🍱Aide-nous à ravir tes papilles !🌶️</h1>
       <button onClick={() => props.modifyIndex(2)}>COMMENCER</button>
     </div>
   );
